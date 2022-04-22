7a265bc8150f5c165466fe6f924c53385d2ab838
xanqus <sbk98031816@gmail.com>
2022-04-22 16:28:39 +0900

    edit innerText error

--- src/components/InfospotListItem.jsx
+++ src/components/InfospotListItem.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
 const InfospotListItem = ({ index, infospot, setCurrentInfospotIndex }) => {
-  return (
-    <div
-      onClick={() => {
-        setCurrentInfospotIndex(index);
-        infospot.focus(1000, function (k) {
-          return k === 1 ? 1 : 1 - Math.pow(2, -10 * k);
-        });
-      }}
-    >
-      {infospot.element.innerText}
-    </div>
-  );
+  try {
+    return (
+      <div
+        onClick={() => {
+          setCurrentInfospotIndex(index);
+          infospot.focus(1000, function (k) {
+            return k === 1 ? 1 : 1 - Math.pow(2, -10 * k);
+          });
+        }}
+      >
+        {infospot.element.innerText}
+      </div>
+    );
+  } catch (e) {
+    console.log(e);
+    return <div></div>;
+  }
 };
 
 export default InfospotListItem;
--- src/components/PanoListItem.jsx
+++ src/components/PanoListItem.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
 const PanoListItem = ({ viewer, panorama, index, setCurrentPanoIndex }) => {
-  return (
-    <div
-      onClick={() => {
-        viewer.setPanorama(panorama);
-        setCurrentPanoIndex(index);
-      }}
-    >
-      {index}번 파노라마
-    </div>
-  );
+  try {
+    return (
+      <div
+        onClick={() => {
+          viewer.setPanorama(panorama);
+          setCurrentPanoIndex(index);
+        }}
+      >
+        {index}번 파노라마
+      </div>
+    );
+  } catch (e) {
+    console.log(e);
+    return <div></div>;
+  }
 };
 
 export default PanoListItem;
